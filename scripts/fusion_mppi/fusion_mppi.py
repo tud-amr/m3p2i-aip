95a9e9472e1ac7f098368975a4af009b6b782fc1
Frankie Zhang <Y.ZHANG-130@student.tudelft.nl>
2022-11-10 15:22:56 +0100

    Use fusion_mppi to change costs at run time and for later development of combining prior controllers

---
+++ scripts/fusion_mppi/fusion_mppi.py
@@ -0,0 +1,81 @@
+import pytorch_mppi.mppi as mppi
+import torch
+from isaacgym import gymtorch
+from utils import sim_init
+
+class FUSION_MPPI(mppi.MPPI):
+    def __init__(self, dynamics, running_cost, nx, noise_sigma, num_samples=100, horizon=15, device="cpu", 
+                    terminal_state_cost=None, 
+                    lambda_=1, 
+                    noise_mu=None, 
+                    u_min=None, 
+                    u_max=None, 
+                    u_init=None, 
+                    U_init=None, 
+                    u_scale=1, 
+                    u_per_command=1, 
+                    step_dependent_dynamics=False, 
+                    rollout_samples=1, 
+                    rollout_var_cost=0, 
+                    rollout_var_discount=0.95, 
+                    sample_null_action=False, 
+                    noise_abs_cost=False):
+        super().__init__(dynamics, running_cost, nx, noise_sigma, num_samples, horizon, device, 
+                    terminal_state_cost, 
+                    lambda_, 
+                    noise_mu, 
+                    u_min, 
+                    u_max, 
+                    u_init, 
+                    U_init, 
+                    u_scale, 
+                    u_per_command, 
+                    step_dependent_dynamics, 
+                    rollout_samples, 
+                    rollout_var_cost, 
+                    rollout_var_discount, 
+                    sample_null_action, 
+                    noise_abs_cost)
+        self.gym = None
+        self.sim = None
+        self.num_envs = num_samples
+    
+    def update_gym(self, gym, sim):
+        self.gym = gym
+        self.sim = sim
+
+    @mppi.handle_batch_input
+    def _dynamics(self, state, u, t):
+        # return self.F(state, u, t) if self.step_dependency else self.F(state, u)
+        self.gym.set_dof_velocity_target_tensor(self.sim, gymtorch.unwrap_tensor(u))
+        self.gym.simulate(self.sim)
+        self.gym.fetch_results(self.sim, True)
+        self.gym.refresh_dof_state_tensor(self.sim)
+        dof_states, _, _, _ = sim_init.acquire_states(self.gym, self.sim, print_flag=False)
+        res = torch.clone(dof_states).view(-1, 4)
+        return res
+
+    @mppi.handle_batch_input
+    def _running_cost(self, state, u):
+        # return self.running_cost(state, u)
+        # State: for each environment, the current state containing position and velocity
+        # Action: same but for control input
+        
+        state_pos = torch.cat((state[:, 0].unsqueeze(1), state[:, 2].unsqueeze(1)), 1)
+        task_cost = torch.linalg.norm(state_pos - torch.tensor([3, -3], device="cuda:0"), axis=1)
+        
+        control_cost = torch.sum(torch.square(u),1)
+        w_u = 0.01
+        # Contact forces
+        _net_cf = self.gym.acquire_net_contact_force_tensor(self.sim)
+        net_cf = gymtorch.wrap_tensor(_net_cf)
+        _net_cf = self.gym.refresh_net_contact_force_tensor(self.sim)
+        # Take only forces in x,y in modulus for each environment. Avoid all collisions
+        net_cf = torch.sum(torch.abs(torch.cat((net_cf[:, 0].unsqueeze(1), net_cf[:, 1].unsqueeze(1)), 1)),1)
+        coll_cost = torch.sum(net_cf.reshape([self.num_envs, int(net_cf.size(dim=0)/self.num_envs)]), 1)
+        w_c = 10000 # Weight for collisions
+        # Binary check for collisions. So far checking all collision of all actors. TODO: check collision of robot body only       
+        coll_cost[coll_cost>0.1] = 1
+        coll_cost[coll_cost<=0.1] = 0
+        
+        return  task_cost + w_u*control_cost + w_c*coll_cost
--- scripts/mppi_point.py
+++ scripts/mppi_point.py
@@ -2,7 +2,7 @@ from isaacgym import gymapi
 from isaacgym import gymutil
 from isaacgym import gymtorch
 import torch
-from pytorch_mppi import mppi
+from fusion_mppi import mppi, fusion_mppi
 from utils import env_conf, sim_init, data_transfer
 import time
 import copy
@@ -21,51 +21,10 @@ gym, sim, viewer, envs, robot_handles = sim_init.make(allow_viewer, num_envs, sp
 # Acquire states
 dof_states, num_dofs, num_actors, root_states = sim_init.acquire_states(gym, sim, print_flag=False)
 
-def mppi_dynamics(input_state, action, t):
-    gym.set_dof_velocity_target_tensor(sim, gymtorch.unwrap_tensor(action))
-    gym.simulate(sim)
-    gym.fetch_results(sim, True)
-    gym.refresh_dof_state_tensor(sim)
-
-    if allow_viewer:
-        gym.step_graphics(sim)
-        gym.draw_viewer(viewer, sim, False)
-        gym.sync_frame_time(sim)
-
-    res = torch.clone(dof_states).view(-1, 4)
-    return res
-
-def running_cost(state, action):
-    # State: for each environment, the current state containing position and velocity
-    # Action: same but for control input
-    
-    state_pos = torch.cat((state[:, 0].unsqueeze(1), state[:, 2].unsqueeze(1)), 1)
-    task_cost = torch.linalg.norm(state_pos - torch.tensor([3, -3], device="cuda:0"), axis=1)
-    
-    control_cost = torch.sum(torch.square(action),1)
-    w_u = 0.01
-    # Contact forces
-    _net_cf = gym.acquire_net_contact_force_tensor(sim)
-    net_cf = gymtorch.wrap_tensor(_net_cf)
-    _net_cf = gym.refresh_net_contact_force_tensor(sim)
-    # Take only forces in x,y in modulus for each environment. Avoid all collisions
-    net_cf = torch.sum(torch.abs(torch.cat((net_cf[:, 0].unsqueeze(1), net_cf[:, 1].unsqueeze(1)), 1)),1)
-    coll_cost = torch.sum(net_cf.reshape([num_envs, int(net_cf.size(dim=0)/num_envs)]), 1)
-    w_c = 10000 # Weight for collisions
-    # Binary check for collisions. So far checking all collision of all actors. TODO: check collision of robot body only       
-    coll_cost[coll_cost>0.1] = 1
-    coll_cost[coll_cost<=0.1] = 0
-    
-    return  task_cost + w_u*control_cost + w_c*coll_cost
-
-def terminal_state_cost(states, actions):
-    # States: for each environment and for the whole time horizon, the state trajectory containing position and velocity
-    # Actions: same but for control input
-    return torch.zeros(num_envs, device="cuda:0")
-
-mppi = mppi.MPPI(
-    dynamics=mppi_dynamics, 
-    running_cost=running_cost, 
+# Creater mppi object
+mppi = fusion_mppi.FUSION_MPPI(
+    dynamics=None, 
+    running_cost=None, 
     nx=2, 
     noise_sigma = torch.tensor([[2, 0], [0, 2]], device="cuda:0", dtype=torch.float32),
     num_samples=num_envs, 
@@ -75,7 +34,7 @@ mppi = mppi.MPPI(
     u_max=torch.tensor([3.0, 3.0]),
     u_min=torch.tensor([-3.0, -3.0]),
     step_dependent_dynamics=True,
-    terminal_state_cost=terminal_state_cost
+    terminal_state_cost=None
     )
 
 # Make sure the socket does not already exist
@@ -114,6 +73,9 @@ with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
             gym.set_dof_state_tensor(sim, gymtorch.unwrap_tensor(s))
             gym.set_actor_root_state_tensor(sim, gymtorch.unwrap_tensor(_root_states))
             sim_init.refresh_states(gym, sim)
+
+            # Update gym in mppi
+            mppi.update_gym(gym, sim)
 
             # Compute optimal action and send to real simulator
             action = mppi.command(s)
