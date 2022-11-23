bbead58de9b838a66b7984b5157f08c0eb89ff98
Chadi Salmi <c.salmi@tudelft.nl>
2022-11-23 14:13:08 +0100

    More informed time logging

--- scripts/sim.py
+++ scripts/sim.py
@@ -1,4 +1,3 @@
-
 from isaacgym import gymapi
 from isaacgym import gymutil
 from isaacgym import gymtorch
@@ -6,14 +5,17 @@ import torch
 from pytorch_mppi import mppi
 from utils import env_conf, sim_init
 import time
+import numpy as np
 torch.set_printoptions(precision=3, sci_mode=False, linewidth=160)
 
 # Decide if you want a viewer or headless
 allow_viewer = True
+visualize_rollouts = True
 
 ## Adding Point robot
 num_envs = 1 
 spacing = 10.0
+dt = 0.1
 
 robot = "boxer"               # choose from "point_robot", "boxer", "albert"
 environment_type = "normal"            # choose from "normal", "battery"
@@ -47,23 +49,49 @@ def torch_to_bytes(t: torch.Tensor) -> bytes:
 
 def bytes_to_torch(b: bytes) -> torch.Tensor:
     buff = io.BytesIO(b)
+    buff.seek(0)
     return torch.load(buff)
     
 
+t4 = time.monotonic()
 with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
     s.connect(server_address)
     while viewer is None or not gym.query_viewer_has_closed(viewer):
-        # Take saved real_state in correct format for mppi.
-        # s = saved_dof_state.view(-1, 4)[0] # [x, v_x, y, v_y]
-
+        t1 = time.monotonic()
         # Compute mppi action. This will internally use the simulator to rollout the dynamics.
         s.sendall(torch_to_bytes(dof_state))
         data = s.recv(1024)
 
         s.sendall(torch_to_bytes(actor_root_state))
-        b = s.recv(1024)
-        action = bytes_to_torch(b)
+        b = s.recv(2048)
+        actions = bytes_to_torch(b)
+        t2 = time.monotonic()
 
+        # Clear lines at the beginning
+        gym.clear_lines(viewer)
+
+        # Send message and receive rollout states
+        if visualize_rollouts:
+            s.sendall(b"Visualize rollouts")
+            rollouts = s.recv(2**18)
+            rollouts = bytes_to_torch(rollouts)
+
+            height = 0.2
+            line_length = rollouts.size()[1] - 1
+            blue = np.array([[0, 0, 255]], dtype='float32').repeat(line_length, axis=0)
+            green = np.array([[0, 255, 0]], dtype='float32').repeat(line_length, axis=0)
+            for i, traj in enumerate(rollouts):
+                line_array = torch.cat((
+                    torch.cat((traj[:-1], torch.ones(line_length, 1, device=torch.device('cuda:0'))*height), axis=1),
+                    torch.cat((traj[1:], torch.ones(line_length, 1, device=torch.device('cuda:0'))*height), axis=1)),
+                    axis=1
+                )
+                if i == 0:
+                    gym.add_lines(viewer, envs[0], line_length, line_array.cpu().numpy(), blue)
+                else:
+                    gym.add_lines(viewer, envs[0], line_length, line_array.cpu().numpy(), green)
+
+        action = actions[0]
         if robot == 'boxer':
             r = 0.08
             L = 2*0.157
@@ -82,23 +110,20 @@ with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
         gym.refresh_dof_state_tensor(sim)
         gym.refresh_rigid_body_state_tensor(sim)
 
+        t3 = time.monotonic()
+
         if viewer is not None:
             # Step rendering
             gym.step_graphics(sim)
             gym.draw_viewer(viewer, sim, False)
-            gym.sync_frame_time(sim)
 
+            sleep_time = dt - (t3-t1)
+            if sleep_time > 0:
+                time.sleep(sleep_time)
 
-        # time logging
-        t = gym.get_elapsed_time(sim)
-        if t >= next_fps_report:
-            t2 = gym.get_elapsed_time(sim)
-            fps = frame_count / (t2 - t1)
-            print("FPS %.1f (%.1f)" % (fps, fps * num_envs))
-            frame_count = 0
-            t1 = gym.get_elapsed_time(sim)
-            next_fps_report = t1 + 2.0
-        frame_count += 1
+        t_prev = t4
+        t4 = time.monotonic()
+        print(f"FPS: {1/(t4-t_prev):.2f}, MPPI: {1e3*(t2-t1):.0f} ms, Simulation: {1e3*(t3-t2):.0f} ms, Graphics: {1e3*(t4-t3):.0f} ms")
 
 
 gym.destroy_viewer(viewer)
