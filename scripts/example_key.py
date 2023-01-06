216b147fc124cc11019af27e4275a5d7322df4a8
corradopezzato <c.pezzato@tudelft.nl>
2023-01-06 14:17:58 +0100

    adding heijn

--- scripts/example_key.py
+++ scripts/example_key.py
@@ -8,7 +8,7 @@ from utils import sim_init, skill_utils
 allow_viewer = True
 num_envs = 1
 spacing = 10.0
-robot = "point_robot"                     # "point_robot", "boxer", "husky", and "albert"
+robot = "heijn"                     # "point_robot", "boxer", "husky", "albert", and heijn
 environment_type = "normal"            # choose from "normal", "battery"
 control_type = "vel_control"        # choose from "vel_control", "pos_control", "force_control"
 
@@ -36,15 +36,15 @@ while viewer is None or not gym.query_viewer_has_closed(viewer):
     sim_init.step(gym, sim)
     sim_init.refresh_states(gym, sim)
 
-    actor_root_state = gymtorch.wrap_tensor(gym.acquire_actor_root_state_tensor(sim)) # [num_envs*16, 13]
-    root_positions = torch.reshape(actor_root_state[:, 0:2], (num_envs, actors_per_env, 2)) # [num_envs, 16, 2]
-    dof_pos = dof_states[:,0].reshape([num_envs, 2]) # [num_envs, 2]
+    # actor_root_state = gymtorch.wrap_tensor(gym.acquire_actor_root_state_tensor(sim)) # [num_envs*16, 13]
+    # root_positions = torch.reshape(actor_root_state[:, 0:2], (num_envs, actors_per_env, 2)) # [num_envs, 16, 2]
+    # dof_pos = dof_states[:,0].reshape([num_envs, 2]) # [num_envs, 2]
     
-    if suction_active:
-        # Simulation of a magnetic/suction effect to attach to the box
-        suction_force, _, _ = skill_utils.calculate_suction(root_positions[:, block_index, :], dof_pos, num_envs, kp_suction, block_index, bodies_per_env)
-        # Apply suction/magnetic force
-        gym.apply_rigid_body_force_tensors(sim, gymtorch.unwrap_tensor(torch.reshape(suction_force, (num_envs*bodies_per_env, 3))), None, gymapi.ENV_SPACE)
+    # if suction_active:
+    #     # Simulation of a magnetic/suction effect to attach to the box
+    #     suction_force, _, _ = skill_utils.calculate_suction(root_positions[:, block_index, :], dof_pos, num_envs, kp_suction, block_index, bodies_per_env)
+    #     # Apply suction/magnetic force
+    #     gym.apply_rigid_body_force_tensors(sim, gymtorch.unwrap_tensor(torch.reshape(suction_force, (num_envs*bodies_per_env, 3))), None, gymapi.ENV_SPACE)
 
     # Respond to keyboard
     sim_init.keyboard_control(gym, sim, viewer, robot, num_dofs, num_envs, dof_states, control_type)
