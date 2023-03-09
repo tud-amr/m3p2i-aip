e96d17b3df5dbffe3242f1d21d5507aad3242989
Frankie Zhang <Y.ZHANG-130@student.tudelft.nl>
2023-03-09 11:47:22 +0100

    Add example file to generate parallel actions

---
+++ scripts/examples/example_aif_parallel.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Simple example to create an AI agent which selects parallel actions to satisfy task needs
+
+import numpy as np
+import sys
+sys.path.append('../')
+from active_inference import ai_agent, isaac_state_action_templates, parallel_action_selection
+import time
+
+## Initialization
+# ----------------- 
+# Define the required mdp structures from the templates
+mdp_isAt = isaac_state_action_templates.MDPIsAt() 
+mdp_isBlockAt = isaac_state_action_templates.MDPIsBlockAt() 
+mdp_isLocFree = isaac_state_action_templates.MDPIsLocFree() 
+mdp_isCloseTo = isaac_state_action_templates.MDPIsCloseTo() 
+
+ai_agent_task = [ai_agent.AiAgent(mdp_isAt), ai_agent.AiAgent(mdp_isBlockAt), ai_agent.AiAgent(mdp_isLocFree), ai_agent.AiAgent(mdp_isCloseTo)]
+
+start_time = time.time()
+ai_agent_task[1].set_preferences(np.array([[1.], [0.]]))
+
+# Loop for the execution of the task, ideally this will be given by the tick of a BT
+for i in range(15):
+    if i < 5:
+        obs = ['null', 1, 0, 1]
+    if i>= 5 and i < 10:
+        obs = ['null', 1, 0, 0]
+    if i>= 10 and i < 15:
+        obs = ['null', 1, 0, 0]    
+
+    outcome, curr_plan = parallel_action_selection.par_act_sel(ai_agent_task, obs)
+    print(outcome)
+    print('Current plan', curr_plan)
