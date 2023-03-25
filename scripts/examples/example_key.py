d9b3a1ab71044039a0c30fc263b9fc801b6699be
Frankie Zhang <Y.ZHANG-130@student.tudelft.nl>
2023-03-25 18:01:00 +0100

    Fix the issue of pull among robots. This is due to incorrect retriving pos data of different robots

--- scripts/examples/example_key.py
+++ scripts/examples/example_key.py
@@ -10,7 +10,7 @@ from utils import sim_init, skill_utils
 allow_viewer = True
 num_envs = 1
 spacing = 10.0
-robot = "point_robot"                     # "point_robot", "boxer", "husky", "albert", and heijn
+robot = "point_robot"                     # "point_robot", "boxer", "husky", "albert", and "heijn"
 environment_type = "normal"            # choose from "normal", "battery"
 control_type = "vel_control"        # choose from "vel_control", "pos_control", "force_control"
 
@@ -28,7 +28,8 @@ count = 0
 gym, sim, viewer, envs, robot_handles = sim_init.make(allow_viewer, num_envs, spacing, robot, environment_type, control_type)
 
 # Acquire states
-dof_states, num_dofs, num_actors, _ = sim_init.acquire_states(gym, sim, print_flag=False)
+dof_states, num_dofs, num_actors, root_states = sim_init.acquire_states(gym, sim, print_flag=False)
+dofs_per_robot = int(num_dofs/num_envs)
 actors_per_env = int(num_actors/num_envs)
 bodies_per_env = gym.get_env_rigid_body_count(envs[0])
 
@@ -38,15 +39,19 @@ while viewer is None or not gym.query_viewer_has_closed(viewer):
     sim_init.step(gym, sim)
     sim_init.refresh_states(gym, sim)
 
-    # actor_root_state = gymtorch.wrap_tensor(gym.acquire_actor_root_state_tensor(sim)) # [num_envs*16, 13]
-    # root_positions = torch.reshape(actor_root_state[:, 0:2], (num_envs, actors_per_env, 2)) # [num_envs, 16, 2]
-    # dof_pos = dof_states[:,0].reshape([num_envs, 2]) # [num_envs, 2]
+    # Get 2D pos of block and robot
+    block_pos = root_states.reshape(num_envs, actors_per_env, 13)[:, block_index, :2] # [num_envs, 2]
+    if robot in ["boxer", "albert", "husky"]:
+        robot_pos = root_states.reshape(num_envs, actors_per_env, 13)[:, -1, :2] # [num_envs, 2]
+    elif robot in ["point_robot", "heijn"]:
+        robot_pos = dof_states[:,0].reshape([num_envs, dofs_per_robot])[:, :2] # [num_envs, 2]
     
-    # if suction_active:
-    #     # Simulation of a magnetic/suction effect to attach to the box
-    #     suction_force, _, _ = skill_utils.calculate_suction(root_positions[:, block_index, :], dof_pos, num_envs, kp_suction, block_index, bodies_per_env)
-    #     # Apply suction/magnetic force
-    #     gym.apply_rigid_body_force_tensors(sim, gymtorch.unwrap_tensor(torch.reshape(suction_force, (num_envs*bodies_per_env, 3))), None, gymapi.ENV_SPACE)
+    if suction_active:
+        # Simulation of a magnetic/suction effect to attach to the box
+        suction_force, _, _ = skill_utils.calculate_suction(block_pos, robot_pos, num_envs, kp_suction, block_index, bodies_per_env)
+        # print('suc', suction_force)
+        # Apply suction/magnetic force
+        gym.apply_rigid_body_force_tensors(sim, gymtorch.unwrap_tensor(torch.reshape(suction_force, (num_envs*bodies_per_env, 3))), None, gymapi.ENV_SPACE)
 
     # Respond to keyboard
     sim_init.keyboard_control(gym, sim, viewer, robot, num_dofs, num_envs, dof_states, control_type)
--- scripts/utils/skill_utils.py
+++ scripts/utils/skill_utils.py
@@ -6,13 +6,15 @@ def calculate_suction(block_pos, robot_pos, num_envs, kp_suction, block_index, b
     dir_vector = block_pos - robot_pos # [num_envs, 2]
     magnitude = 1/torch.linalg.norm(dir_vector, dim=1) # [num_envs]
     magnitude = magnitude.reshape([num_envs, 1])
+    # print('robo', robot_pos)
+    # print('mag', magnitude)
 
     # Form the suction force
     unit_force = dir_vector*magnitude  # [num_envs, 2] Same as the unit direction of pulling force
     forces = torch.zeros((num_envs, bodies_per_env, 3), dtype=torch.float32, device='cuda:0', requires_grad=False)
     
     # Start suction only when close
-    mask = magnitude[:, :] > 2
+    mask = magnitude[:, :] > 1.5
     mask = mask.reshape(num_envs)
     # Force on the block
     forces[mask, block_index, 0] = -kp_suction*unit_force[mask, 0]
