c3d6e2d4526499088d9c6201e4eb223ab9410925
Frankie Zhang <Y.ZHANG-130@student.tudelft.nl>
2023-06-06 17:15:32 +0200

    Draw the plots of case 3, halton push pull hybrid for one corner

--- scripts/plot/plot_point.py
+++ scripts/plot/plot_point.py
@@ -9,14 +9,20 @@ import plotly.graph_objects as go
 
 file_path_1 = path_utils.get_plot_path() +'/point/case2_halton_push_coll.npy'
 file_path_2 = path_utils.get_plot_path() +'/point/case2_halton_pull_coll.npy'
+file_path_3 = path_utils.get_plot_path() +'/point/case3_halton_push_corner1.npy'
+file_path_4 = path_utils.get_plot_path() +'/point/case3_halton_pull_corner1.npy'
+file_path_5 = path_utils.get_plot_path() +'/point/case3_halton_hybrid_corner1.npy'
 push_data = np.load(file_path_1, mmap_mode="r")
 pull_data = np.load(file_path_2, mmap_mode="r")
-n = int(pull_data.size/19)
+push_data_c1 = np.load(file_path_3, mmap_mode="r")
+pull_data_c1 = np.load(file_path_4, mmap_mode="r")
+hybrid_data_c1 = np.load(file_path_5, mmap_mode="r")
+n = int(pull_data_c1.size/19)
 # print(push_data)
 print(n)
 
 # Compute cost of distance and quaternion
-def compute_cost(data_array):
+def compute_cost(data_array, n):
     quat_cost = skill_utils.get_general_ori_cube2goal(torch.tensor(data_array[:, 8:12]), 
                                              torch.tensor([0, 0, 0, 1]).repeat(n).view(n, 4))
     dist_disp = data_array[:, 1:3] - data_array[:, 12:14]
@@ -24,12 +30,21 @@ def compute_cost(data_array):
     quat_cost = quat_cost.cpu().detach().numpy()
     return pos_cost, quat_cost
 
-push_pos_cost, push_quat_cost = compute_cost(push_data)
+push_pos_cost, push_quat_cost = compute_cost(push_data, 60)
 push_dyn_obs_coll = push_data[:, 17]
 push_task_time = push_data[:, 18]
-pull_pos_cost, pull_quat_cost = compute_cost(pull_data)
+pull_pos_cost, pull_quat_cost = compute_cost(pull_data, 60)
 pull_dyn_obs_coll = pull_data[:, 17]
 pull_task_time = pull_data[:, 18]
+push_c1_pos_cost, push_c1_quat_cost = compute_cost(push_data_c1, 20)
+push_c1_dyn_obs_coll = push_data_c1[:, 17]
+push_c1_task_time = push_data_c1[:, 18]
+pull_c1_pos_cost, pull_c1_quat_cost = compute_cost(pull_data_c1, 20)
+pull_c1_dyn_obs_coll = pull_data_c1[:, 17]
+pull_c1_task_time = pull_data_c1[:, 18]
+hybrid_c1_pos_cost, hybrid_c1_quat_cost = compute_cost(hybrid_data_c1, 20)
+hybrid_c1_dyn_obs_coll = hybrid_data_c1[:, 17]
+hybrid_c1_task_time = hybrid_data_c1[:, 18]
 
 # Box plot
 fig = go.Figure()
@@ -38,7 +53,24 @@ fig.add_trace(go.Box(y=np.concatenate((push_pos_cost, push_quat_cost, push_dyn_o
 fig.add_trace(go.Box(y=np.concatenate((pull_pos_cost, pull_quat_cost, pull_dyn_obs_coll, pull_task_time)), x=label_x, name="pull", marker_color='#FF851B'))
 
 fig.update_layout(
-    title = 'Metrics',
+    title = 'Results of Normal Case',
+    title_x=0.5,
+    # yaxis_title='Displacements',
+    boxmode='group', # group together boxes of the different traces for each value of x
+    boxgroupgap=0.3, # update
+    boxgap=0
+)
+fig.show() 
+
+# Box plot
+fig = go.Figure()
+label_x = ['pos_error']*20+['ori_error']*20+['task time']*20
+fig.add_trace(go.Box(y=np.concatenate((push_c1_pos_cost, push_c1_quat_cost, push_c1_task_time)), x=label_x, name="push", marker_color='#FF4136'))
+fig.add_trace(go.Box(y=np.concatenate((pull_c1_pos_cost, pull_c1_quat_cost, pull_c1_task_time)), x=label_x, name="pull", marker_color='#FF851B'))
+fig.add_trace(go.Box(y=np.concatenate((hybrid_c1_pos_cost, hybrid_c1_quat_cost, hybrid_c1_task_time)), x=label_x, name="hybrid", marker_color='#3D9970'))
+
+fig.update_layout(
+    title = 'Results of One Corner Case ',
     title_x=0.5,
     # yaxis_title='Displacements',
     boxmode='group', # group together boxes of the different traces for each value of x
