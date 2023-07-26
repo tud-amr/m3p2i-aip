47347deffd5a8112036b689de068a10daca0b81d
Frankie Zhang <Y.ZHANG-130@student.tudelft.nl>
2023-07-26 16:05:06 +0200

    Now AIF works, but place action does not or cannot model the precondition of getting_close_to_pre_place

--- scripts/active_inference/state_action_templates.py
+++ scripts/active_inference/state_action_templates.py
@@ -5,16 +5,17 @@ import numpy as np
 
 class MDPIsAt:
     def __init__(self): 
-        self.state_name = 'isAt'                            # This is the general name the class refers to
-        self.state_names = ['at_goal', 'not_at_goal']                 # These are the names a certain battery state can have
-        self.action_names = ['idle', 'move_to']    # These are the names of the actions for internal needs
+        self.state_name = 'isAt'                                # This is the general name the class refers to
+        self.state_names = ['at_goal', 'not_at_goal']           # These are the names a certain battery state can have
+        self.action_names = ['idle', 'move_to']                 # These are the names of the actions for internal needs
 
-        self.V = np.array([0, 1])  # Allowable policies, it indicates policies of depth 1
-        self.B = np.zeros((2, 2, 2))  # Allowable actions initiation
+        self.V = np.array([0, 1])           # Allowable policies, it indicates policies of depth 1
+        self.B = np.zeros((2, 2, 2))        # Allowable actions initiation
+
         # Transition matrices
         # ----------------------------------------------------------
         self.B[:, :, 0] = np.eye(2)  # Idle action
-        self.B[:, :, 1] = np.array([[1, 1],  # move(loc): a_moveBase makes isAt true
+        self.B[:, :, 1] = np.array([[1, 1],         # move_to
                                     [0, 0]])
 
         # Preconditions of the actions above
@@ -40,21 +41,22 @@ class MDPIsAt:
 
 class MDPIsHolding:
     def __init__(self):
-        self.state_name = 'isHolding'                            # This is the general name the class refers to
-        self.state_names = ['holding_obj', 'not_holding_obj']                 # These are the names a certain battery state can have
-        self.action_names = ['idle', 'pick', 'place_somewhere']    # These are the names of the actions for internal needs
+        self.state_name = 'isHolding'                            
+        self.state_names = ['holding_obj', 'not_holding_obj']               
+        self.action_names = ['idle', 'pick', 'place_somewhere']  
 
-        self.V = np.array([0, 1, 2])  # Allowable policies, it indicates policies of depth 1
-        self.B = np.zeros((2, 2, 3))  # Allowable actions initiation
+        self.V = np.array([0, 1, 2])  
+        self.B = np.zeros((2, 2, 3))
+        
         # Transition matrices
         # ----------------------------------------------------------
         self.B[:, :, 0] = np.eye(2)  # Idle action
-        self.B[:, :, 1] = np.array([[1, 1],  # Pick action
+        self.B[:, :, 1] = np.array([[1, 1],  # pick action
                                     [0, 0]])
-        self.B[:, :, 2] = np.array([[0, 0],  # place_somewhere action
+        self.B[:, :, 2] = np.array([[0, 0],  # Place_somewhere action
                                     [1, 1]])
         # Preconditions of the actions above
-        self.preconditions = [['none'], ['not_holding_obj', 'reachable', 'visible'], ['none']] # [Idle precondition], [pick preconditions], [place precondition]  
+        self.preconditions = [['none'], ['not_holding_obj', 'reachable', 'visible'], ['none']]  
 
         # Likelihood matrix matrices
         # ----------------------------------------------------------
@@ -75,9 +77,9 @@ class MDPIsHolding:
 
 class MDPIsReachable:
     def __init__(self): 
-        self.state_name = 'isReachable'                            # This is the general name the class refers to
-        self.state_names = ['reachable', 'not_reachable']                 # These are the names a certain battery state can have
-        self.action_names = ['idle', 'reach']    # These are the names of the actions for internal needs
+        self.state_name = 'isReachable'                            
+        self.state_names = ['reachable', 'not_reachable']                
+        self.action_names = ['idle', 'reach']    
 
         self.V = np.array([0, 1])  # Allowable policies, it indicates policies of depth 1
         self.B = np.zeros((2, 2, 2))  # Allowable actions initiation
@@ -111,12 +113,12 @@ class MDPIsReachable:
 
 class MDPIsVisible:
     def __init__(self): 
-        self.state_name = 'isVisible'                            # This is the general name the class refers to
-        self.state_names = ['visible', 'not_visible']                 # These are the names a certain battery state can have
-        self.action_names = ['idle', 'look_around']    # These are the names of the actions for internal needs
+        self.state_name = 'isVisible'                           
+        self.state_names = ['visible', 'not_visible']               
+        self.action_names = ['idle', 'look_around']   
 
-        self.V = np.array([0, 1])  # Allowable policies, it indicates policies of depth 1
-        self.B = np.zeros((2, 2, 2))  # Allowable actions initiation
+        self.V = np.array([0, 1])  
+        self.B = np.zeros((2, 2, 2))
         # Transition matrices
         # ----------------------------------------------------------
         self.B[:, :, 0] = np.eye(2)             # Idle action
@@ -147,12 +149,13 @@ class MDPIsVisible:
 
 class MDPIsPlacedAt:
     def __init__(self):
-        self.state_name = 'isInBasket'                                                  # This is the general name the class refers to
-        self.state_names = ['reach_pre_place', 'not_reach_pre_place']                 # These are the names a certain battery state can have
-        self.action_names = ['idle', 'place']    # These are the names of the actions for internal needs
+        self.state_name = 'isInBasket'                                                  
+        self.state_names = ['placed_in_basket', 'not_placed_in_basket']               
+        self.action_names = ['idle', 'place']   
 
-        self.V = np.array([0, 1])       # Allowable policies, it indicates policies of depth 1
-        self.B = np.zeros((2, 2, 2))    # Allowable actions initiation (idle and place_in_basket)
+        self.V = np.array([0, 1])       
+        self.B = np.zeros((2, 2, 2))
+        
         # Transition matrices
         # ----------------------------------------------------------
         self.B[:, :, 0] = np.eye(2)             # Idle action
@@ -160,7 +163,7 @@ class MDPIsPlacedAt:
                                     [0, 0]])
         # Preconditions of the actions above
         # ----------------------------------------------------------
-        self.preconditions = [['none'], ['holding_obj', 'reach_pre_place']]    # [idle precondition], [place_in_backet_obj]                  
+        self.preconditions = [['none'], ['holding_obj']]    # [idle precondition], [place_in_backet_obj]                  
 
 
         # Likelihood matrix matrices
--- scripts/examples/test.py
+++ scripts/examples/test.py
@@ -18,19 +18,9 @@ mdp_isVisible = state_action_templates.MDPIsVisible()
 
 # Agent with following states [isAt, isHolding, isReachable, isPlacedAt, isVisible]
 ai_agent_task = [ai_agent.AiAgent(mdp_isAt), ai_agent.AiAgent(mdp_isHolding), ai_agent.AiAgent(mdp_isReachable), ai_agent.AiAgent(mdp_isPlacedAt), ai_agent.AiAgent(mdp_isVisible)]
-
+ai_agent_task[3].set_preferences(np.array([[1.], [0.]]))
 # Loop for the execution of the task, ideally this will be given by the tick of a BT
-for i in range(20):
-    # Set preference for a particular state to be achieved. Follow indexing in ai_agent_task [isAt, isHolding, isReachable, isPlacedAt, isVisible]
-    ai_agent_task[3].set_preferences(np.array([[1.], [0.]]))
-    #ai_agent_task[1].set_preferences(np.array([[2.], [0.]]))
-    ai_agent_task[1]._mdp.D = (np.array([[0], [1]]))
-
-    # Get the parameters of the object to be holding to define a suitable observation
-    # most likely from the parameter server (TODO add the parameters from the BT)
-
-    # Set the observation from the current readings, (TODO) the logic of the observations need to be specified for the task and the parameters passed by the BT
-    # in terms of products. When an observation is unavailable set it to 'null'
+for i in range(35):
     if i < 5:
         # should reach
         is_holding = False
@@ -51,6 +41,16 @@ for i in range(20):
         is_holding = True
         is_reachable = True
         close_to_pre_place = True
+    elif i < 25:
+        # should reach
+        is_holding = False
+        is_reachable = False
+        close_to_pre_place = False
+    elif i < 30:
+        # should pick
+        is_holding = False
+        is_reachable = True
+        close_to_pre_place = False
     # Translate to integer observations, do not worry about the negatives, 0 means true
     obs = ['null', int(not is_holding), int(not is_reachable), int(not close_to_pre_place), 0]
     outcome, curr_acti = adaptive_action_selection.adapt_act_sel(ai_agent_task, obs)
